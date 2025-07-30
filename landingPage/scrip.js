c9bcb48805896ab3a41c737427d1d5260eef2ed3
miguelxxx <miguelparreiramedeiros@gmail.com>
2025-07-30 11:38:46 -0300

    Add files via upload

---
+++ landingPage/scrip.js
@@ -0,0 +1,19 @@
+function enviar(){
+    //atribuindo a variaveis constanstes valores digitados
+    document.getElementById('nome').style.border = '1px solid black'
+    document.getElementById('assunto').style.border = '1px solid black' 
+
+ const nome = document.getElementById('nome').value
+ const assunto = document.getElementById('assunto').value
+
+ 
+if(!nome || !assunto){
+    document.getElementById('nome').style.border = '2px solid red'
+    document.getElementById('assunto').style.border = '2px solid red'
+    return
+}
+ 
+const mensagem = `gostaria de entrar em você" \n \nNome: ${nome} \nAssunto: ${assunto}`
+const msg = encodeURIComponent(mensagem)
+window.location.href = `https://wa.me/5541998817974?text=${msg}`
+}
